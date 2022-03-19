472839ac95b323f002b8f9435458ef0088d7d70c
Ashay Jaiswal <ashaywin@gmail.com>
2022-03-19 18:06:46 +0530

    Add darkening on multiple hovers

--- pad.js
+++ pad.js
@@ -36,7 +36,7 @@ function populateGrid(gridCount=10){
         //console.log('Gridcount', gridCount, '  RowCount', rowCount, '  ColumnCount', columncount, '  CurrentGrid: ', totalGrids)
         const newGrid = document.createElement("div");
         newGrid.setAttribute('class', 'box');
-        newGrid.setAttribute('style', `width: ${gridSideSize}px; height: ${gridSideSize}px;`)
+        newGrid.setAttribute('style', `width: ${gridSideSize}px; height: ${gridSideSize}px; filter: brightness(1);`)
         newGrid.addEventListener('mouseover', paintGrid);
 
         if (gridCount % 2 === 0 && rowCount % 2 === 0){
@@ -82,10 +82,45 @@ function clearGrid() {
     populateGrid();
 }
 
+function RGBToHex(rgb) {
+    // Choose correct separator
+    let sep = rgb.indexOf(",") > -1 ? "," : " ";
+    // Turn "rgb(r,g,b)" into [r,g,b]
+    rgb = rgb.substr(4).split(")")[0].split(sep);
+  
+    let r = (+rgb[0]).toString(16),
+        g = (+rgb[1]).toString(16),
+        b = (+rgb[2]).toString(16);
+  
+    if (r.length == 1)
+      r = "0" + r;
+    if (g.length == 1)
+      g = "0" + g;
+    if (b.length == 1)
+      b = "0" + b;
+  
+    return "#" + r + g + b;
+  }
+
 function paintGrid(grid){
     //console.log(grid);
     let chosenColor = colorPicker.value;
-    grid.target.style.background = chosenColor;
+    let gridColor = RGBToHex(grid.target.style.background);
+    console.log(chosenColor, gridColor)
+    if (chosenColor === gridColor){
+
+        let currentBrightness = parseFloat(grid.target.style.filter.replace('brightness(', '').replace(')', ''));
+        if (currentBrightness > 0){
+            currentBrightness =  currentBrightness - 0.1
+            grid.target.style.filter = `brightness(${currentBrightness})`;
+
+        }
+        
+    }else {
+        grid.target.style.background = chosenColor;
+    }
+    
+
 }
 
 
